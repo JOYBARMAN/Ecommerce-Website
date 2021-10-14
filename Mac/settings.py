5f27476cede5384907eeeb1d309cbe2ed497ebb0
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2021-10-14 21:41:35 +0600

    New commit

--- Mac/settings.py
+++ Mac/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Managing Media
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
